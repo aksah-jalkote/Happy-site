70419b297f060274bd5251438c252d785b80dcc3
akash <gitakash16@gmail.com>
2020-08-04 19:46:19 +0530

    reinitilized files

--- src/contact/Main.js
+++ src/contact/Main.js
@@ -1,51 +1,44 @@
 import React from 'react';
-import "./contact.css"
+import './contact.css';
+import secure3 from '../core/images/secure3.svg';
+
 const Main = () => {
   return (
     <div id="Contactme">
-      {/* <div className="container">
-         <div class="row">  */}
-           <div class="col-6 p-5"> 
-            <h1 className="">Fill this form</h1>
-            <form action="" class="mt-5">
-              <div class="form-group">
-                <label for="" class="text-dark">
-                  Name
-                </label>
-                <input type="text" placeholder="Name" class="form-control" />
-              </div>
-              <div class="form-group">
-                <label for="" class="text-dark">
-                  Email
-                </label>
-                <input type="email" placeholder="Email" class="form-control" />
-              </div>
-              <div class="form-group">
-                <label for="" class="text-dark">
-                  Mobile No.
-                </label>
-                <input
-                  type="tel"
-                  placeholder="Mobile No"
-                  class="form-control"
-                />
-              </div>
-              <div class="form-group">
-                <label for="" class="text-dark">
-                  Description
-                </label>
-                <textarea class="form-control" rows="4"></textarea>
-              </div>
-              <div>
-                <button class="btn btn-md btn-block" type="submit">
-                  Submit
-                </button>
-              </div>
-            </form>
-          </div>
-         </div>
-    //   </div>
-    // </div>
+      <form action="" class="mt-5">
+        <h1>Fill this Form</h1>
+        <div class="form-group">
+          <label for="" class="text-dark">
+            Name
+          </label>
+          <input type="text" placeholder="Name" class="form-control" />
+        </div>
+        <div class="form-group">
+          <label for="" class="text-dark">
+            Email
+          </label>
+          <input type="email" placeholder="Email" class="form-control" />
+        </div>
+        <div class="form-group">
+          <label for="" class="text-dark">
+            Mobile No.
+          </label>
+          <input type="tel" placeholder="Mobile No" class="form-control" />
+        </div>
+        <div class="form-group">
+          <label for="" class="text-dark">
+            Description
+          </label>
+          <textarea class="form-control" rows="4"></textarea>
+        </div>
+        <div>
+          <button class="btn btn-md btn-primary btn-block" type="submit">
+            Submit
+          </button>
+        </div>
+      </form>
+      {/* <img src={secure3} className="img-fluid mt-5" /> */}
+    </div>
   );
 };
 
